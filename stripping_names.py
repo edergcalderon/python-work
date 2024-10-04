14fb8717726757422c31564f1d5a423aba9a29ce
edergcalderon <64505582+edergcalderon@users.noreply.github.com>
2024-10-03 18:55:20 -0700

    Add files via upload

---
+++ stripping_names.py
@@ -0,0 +1,15 @@
+name = "\tIrma Lovelace\n"
+
+print("Unmodified:")
+print(name)
+
+print("\nUsing lstrip():")
+print(name.lstrip())
+
+print("nUsing rstrip():")
+print(name.rstrip())
+
+print("nUsing strip():")
+print(name.strip())
+
+# This program removes the whitespace from the beginning and end of the word.abs
