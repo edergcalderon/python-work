fff7d47e709c6bfa6bb002522c9d0d2306478fe3
edergcalderon <64505582+edergcalderon@users.noreply.github.com>
2024-10-05 23:26:52 -0700

    Add files via upload

---
+++ seeing_the_world.py
@@ -0,0 +1,17 @@
+places = ['Chiapas', 'Mexico City', 'Sinaloa', 'Nayarit', 'Oaxaca']
+print(places)
+
+places = ['Chiapas', 'Mexico City', 'Sinaloa', 'Nayarit', 'Oaxaca']
+places.sort()
+print(places)
+
+places = ['Chiapas', 'Mexico City', 'Sinaloa', 'Nayarit', 'Oaxaca']
+places.sort(reverse=True)
+print(places)
+
+places = ['Chiapas', 'Mexico City', 'Sinaloa', 'Nayarit', 'Oaxaca']
+print(places)
+
+places = ['Chiapas', 'Mexico City', 'Sinaloa', 'Nayarit', 'Oaxaca']
+places.reverse()
+print(places)
