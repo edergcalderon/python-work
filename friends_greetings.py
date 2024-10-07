b4b1f79646c461aa097caae841fc8de77142a355
edergcalderon <64505582+edergcalderon@users.noreply.github.com>
2024-10-07 12:40:57 -0700

    Add files via upload

---
+++ friends_greetings.py
@@ -0,0 +1,24 @@
+friends = ['Kevin', 'Michael', 'Penguin', 'Isaiah', 'Austin']
+message = f"Hello, {friends[0].title()}! When are we going to meet up again?"
+
+print(message)
+
+friends = ['Kevin', 'Michael', 'Penguin', 'Isaiah', 'Austin']
+message = f"Hello, {friends[-1].title()}! When are we going to meet up again?"
+
+print(message)
+
+friends = ['Kevin', 'Michael', 'Penguin', 'Isaiah', 'Austin']
+message = f"Hello, {friends[1].title()}! When are we going to meet up again?"
+
+print(message)
+
+friends = ['Kevin', 'Michael', 'Penguin', 'Isaiah', 'Austin']
+message = f"Hello, {friends[2].title()}! When are we going to meet up again?"
+
+print(message)
+
+friends = ['Kevin', 'Michael', 'Penguin', 'Isaiah', 'Austin']
+message = f"Hello, {friends[3].title()}! When are we going to meet up again?"
+
+print(message)
