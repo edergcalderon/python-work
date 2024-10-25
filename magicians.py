7119788f18cc576a0807b7ef927cb5a8eb1a1e46
edergcalderon <64505582+edergcalderon@users.noreply.github.com>
2024-10-25 08:43:31 -0700

    Add files via upload

---
+++ magicians.py
@@ -0,0 +1,37 @@
+# Looping is important because it’s one of the most common ways a computer automates repetitive tasks
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician) 
+
+# Composing a message
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+    
+# Adding second msg and space between msgs
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")
+
+# Writing a ty msg
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")
+
+# Displaying ty msg without indentation
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")
+
+print("Thank you, everyone. That was a great magic show!")
+
+# Lack of indentation causes 'logical error'
+# Only values associated 'Magician'
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+print(f"I can't wait to see your next trick, {magician.title()}.\n")
