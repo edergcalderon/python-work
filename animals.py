0a5a2bb019ec30a7b732c221ca544734f29e2831
edergcalderon <64505582+edergcalderon@users.noreply.github.com>
2025-01-03 23:03:42 -0800

    chapter 4 exercises/

---
+++ animals.py
@@ -0,0 +1,5 @@
+animals = ['dog', 'cat', 'parrot']
+for animal in animals:
+    print(animal)
+    print(f"A {animal.title()} is a great pet and loyal companion.")
+    print(f"I'm a big believer that any of these animals would make great pets, they are all loyal.")
