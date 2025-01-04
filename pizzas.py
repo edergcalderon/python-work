de33e7e8c27b7096cb0e801439557df25ade3710
edergcalderon <64505582+edergcalderon@users.noreply.github.com>
2025-01-03 23:03:13 -0800

    Delete pizzas.py

--- pizzas.py
+++
@@ -1,5 +0,0 @@
-pizzas = ['pepperoni', 'jalapeno', 'pineapple']
-for pizza in pizzas:
-    print(f"I really like {pizza.title()} pizza.")
-    print(f"I can't begin to explain how much I love {pizza.title()} pizza.")
-    print(f"I really love pizza!")
