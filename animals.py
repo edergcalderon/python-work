2ac6a98fead9c13000531b358a9ec7d44a500048
edergcalderon <64505582+edergcalderon@users.noreply.github.com>
2025-01-03 23:04:48 -0800

    Delete animals.py

--- animals.py
+++
@@ -1,5 +0,0 @@
-animals = ['dog', 'cat', 'parrot']
-for animal in animals:
-    print(animal)
-    print(f"A {animal.title()} is a great pet and loyal companion.")
-    print(f"I'm a big believer that any of these animals would make great pets, they are all loyal.")
