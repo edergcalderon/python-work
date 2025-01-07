ebd4c663f933723a30dde04f550658b5fed9b1e0
edergcalderon <64505582+edergcalderon@users.noreply.github.com>
2025-01-06 22:33:57 -0800

    Add files via upload

---
+++ summing_a_million.py
@@ -0,0 +1,5 @@
+numbers = [1, 1000000]
+
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
---
+++ threes.py
@@ -0,0 +1,10 @@
+# Create an empty list to store the multiples of 3
+multiples_of_three = []
+
+# Loop through the numbers from 3 to 30 (inclusive), with a step of 3
+for number in range(3, 31, 3):
+    # Append each number to the list
+    multiples_of_three.append(number)
+
+# Print the list of multiples of 3
+print(multiples_of_three)
