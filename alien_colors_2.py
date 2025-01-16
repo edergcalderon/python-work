dfa8355ab2ed014e9ebba4dec93a500662965ff2
edergcalderon <64505582+edergcalderon@users.noreply.github.com>
2025-01-15 22:07:03 -0800

    Update alien_colors_2.py

--- alien_colors_2.py
+++ alien_colors_2.py
@@ -4,3 +4,10 @@ if alien_color == 'green':
     print("You just earned 5 points!")
 else:
     print("You just earned 10 points!")
+
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+else:
+    print("You just earned 10 points!")
